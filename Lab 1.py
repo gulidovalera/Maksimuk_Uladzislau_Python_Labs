9e51dc96fcb430b194924a4a0a25795e33179ce8
blydislove <blydislove@gmail.com>
2025-04-07 12:11:47 +0300

    Lab_1 added

---
+++ Lab 1.py
@@ -0,0 +1,61 @@
+# Variant 3
+## using 1 def
+def hamming_distance_triplets(s, t):
+    distance = 0
+    for i in range(0, len(s), 3):
+        triplet_s = s[i:i+3]
+        triplet_t = t[i:i+3]
+        for j in range(len(triplet_s)):
+            if triplet_s[j] != triplet_t[j]:
+                distance += 1
+    return distance
+
+s = str(input("Введите первую последовательность: "))
+t = str(input("Введите вторую последовательность: "))
+
+distance = hamming_distance_triplets(s, t)
+
+print(distance)
+## using class + 3 methods
+class HammingTripletCalculator:
+    def __init__(self):
+        self.s=""
+        self.t=""
+
+    def input_sequence(self):
+        self.s = input("Введите первую последовательность:")
+        self.t = input("Введите вторую последовательность:")
+
+    def calculate_distance(self):
+        distance = 0
+        for i in range (0, len(self.s), 3):
+            triplet_s = self.s[i:i+3]
+            triplet_t = self.t[i:i+3]
+            for s_char, t_char in zip(triplet_s,triplet_t):
+                if s_char != t_char:
+                    distance += 1
+        return distance
+
+calculator = HammingTripletCalculator()
+
+## GAGCCTACTAACGGGAT
+## CATCGTAATGACGGCCT
+
+
+
+
+# class + 2 methods
+
+
+# Variant 4
+
+# from itertools import permutations
+#
+# n = int(input())
+#
+# perm_list = list(permutations(range(1, n+1)))
+#
+# print(len(perm_list))
+#
+# for perm in perm_list:
+#     print(*perm)
